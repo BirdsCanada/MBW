9f8c66e1bf55b0873f5a920dc457d5ae3e51bfe7
Danielle Ethier <dethier@birdscanada.org>
2025-04-11 10:42:57 -0400

    Danielle's Code updates to be merged with Main Branch

--- MBW_2024_Trend.R
+++ MBW_2024_Trend.R
@@ -1,35 +1,41 @@
-#Libraries
+##Libraries
+
 library(naturecounts)
 library(tidyr)
-library(dplyr)
 library(unmarked)
-library(ggplot2)
-library(lubridate)
 library(MuMIn)
 library(DHARMa)
+library(performance)
+library(see)
+library(lme4)
+library(sf)
+library(ggspatial)
+library(suncalc)
+library(ARUtools)
+library(glmmTMB)
+library(lme4)    
 
+##Load Data
 
-rm(list=ls())
-par(mfrow=c(1,1),mar=c(5,5,3,2)+0.1,cex.axis=1.5,cex.lab=1.5)
-
-###Full MBW dataset from NatureCounts###
+#Full MBW dataset from NatureCounts###
 #replace with your user name and request_id
 #only need to do once per season
 nc_requests(username = "amyleek")
 MBW.NC <- nc_data_dl(request_id = 252290, fields_set = "extended", username = "amyleek",
                      info = "analysis for HELP data internal")
-
 #write to data folder
 write.csv(MBW.NC, "Data/MBW_2024.csv")
 
+#read from data folder
 ddf<-read.csv("Data/MBW_2024.csv") #pull in data from file for working with
+
+##Data Cleaning
 
 #remove NS data from 2016 and 2017
 ddf <- filter(ddf, subnational2_code != "CA.NS.IN", subnational2_code != "CA.NS.VI")
 
 #get rid of survey with weird start time
 ddf <- filter(ddf, TimeObservationsStarted != 13.3333)
-
 
 #retain only the columns that will be useful for the analysis
 ddf<-ddf %>% select(SurveyAreaIdentifier, RouteIdentifier, ProtocolCode, species_id, CommonName, subnational2_code, survey_year, survey_month, survey_day, TimeObservationsStarted, TimeObservationsEnded, ObservationCount, ObservationDescriptor, ObservationCount2, ObservationDescriptor2, ObservationCount3, ObservationDescriptor3, ObservationCount4, ObservationDescriptor4, 
@@ -38,24 +44,48 @@ ddf<-ddf %>% select(SurveyAreaIdentifier, RouteIdentifier, ProtocolCode, species
 #create doy field
 ddf<-ddf %>% format_dates()
 
+##Data Visualization
+ddf_sf <- st_as_sf(ddf, coords = c("DecimalLatitude", "DecimalLongitude"), crs = 4326)
+
+  plot<-ggplot(data = ddf_sf) +
+    # Select a basemap
+    annotation_map_tile(type = "cartolight", zoom = NULL, progress = "none") +
+    # Plot the points, color-coded by survey_year
+    geom_sf(aes(color = as.factor(survey_year)), size = 1) +
+    # Add a theme with a minimal design and change the font styles, to your preference
+    theme_minimal() +
+       theme(legend.position = "bottom") +
+    # To make the points in the legend larger without affecting map points
+    guides(color = guide_legend(override.aes = list(size = 3))) +
+    # Define the title and axis names
+    labs(title = "MBW Survey Stops",
+         x = "Longitude",
+         y = "Latitude")
+###Need to check your latitude on some of your points. 
+  
+##Data Summary
+  
+#total count of species per year
+sum_sp<-ddf %>% group_by(CommonName, survey_year) %>% summarise(CountTot=sum(ObservationCount, na.rm =TRUE)) %>% filter(!is.na(CommonName))
+sum_sp<-pivot_wider(
+  data = sum_sp,
+  names_from = survey_year,    # Column to use for new column names
+  values_from = CountTot      # Column containing values to fill
+)
+
+#Events
+#Create Events Matrix which includes the colvarites of interest
+all_species_events<-ddf %>% select(SurveyAreaIdentifier, survey_year, survey_month, survey_day, doy, TimeObservationsStarted, DecimalLatitude, DecimalLongitude, ProtocolCode, RouteIdentifier) %>% distinct()
 
 ####NEW ANALYSIS###
 
-#install.packages(c("performance", "see"))
-library(performance)
-library(see)
-library(lme4)
-
-#make year continous
+#make year continuous
 ddf$survey_year <- as.numeric(ddf$survey_year)
 
 #Calculate survey start column. Time since sunrise (~5:30AM sunrise Popple Depot, NB in June, but used 4:15AM to avoid negative numbers.)
 
 #ddf$TimeSinceSunrise <- ddf$TimeObservationsStarted - 4.25
 #ddf$TimeBeforeSunset <- ddf$TimeObservationsStarted - 20
-
-library(suncalc)
-library(ARUtools)
 
 
 #Poisson GLM
@@ -65,8 +95,11 @@ library(ARUtools)
 GLM2 <- glmer(ObservationCount ~ doy + survey_year + ProtocolCode + (1 | RouteIdentifier),
              family = poisson, data = ddf)
 
+#Danielle new suggested model to try
+GLM2 <- glmer(ObservationCount ~  survey_year + ProtocolCode + (1 | SurveyAreaIndentifier),
+              family = poisson, data = ddf)
 
-library(glmmTMB)
+
 GLM3 <- glmmTMB(ObservationCount ~ survey_year + doy + ProtocolCode + (1 | RouteIdentifier),
               family = poisson, data = ddf)
 
@@ -88,25 +121,33 @@ results <- data.frame(Group = integer(),
                       stringsAsFactors = FALSE)
 
 
-sp_ids <- unique(ddf$species_id)
+sp_ids<-unique(ddf$CommonName)
 
 for(m in 1:length(sp_ids)) {
   
    #m<-1 #for testing
   
   sp.ddf<-ddf %>% filter(CommonName==sp_ids[m]) #this will cycle through each species in sp.ids. For testing you can manually set m to 
-  sp.ddf<-sp.ddf %>% select(SurveyAreaIdentifier, RouteIdentifier, survey_year, doy, TimeObservationsStarted)
-  sp.ddf<-left_join(all_species_events, sp.ddf, by=c("SurveyAreaIdentifier", "survey_year")) #you will zero fill in the loop
-    
+  sp.ddf<-sp.ddf %>% select(CommonName, SurveyAreaIdentifier, survey_year, doy, TimeObservationsStarted, ObservationCount)
+  sp.ddf<-left_join(all_species_events, sp.ddf, by=c("SurveyAreaIdentifier", "survey_year", "doy", "TimeObservationsStarted")) #you will zero fill in the loop
+  #Add the 0 to observation count
+  sp.ddf <- sp.ddf %>%
+    mutate(ObservationCount = replace(ObservationCount, is.na(ObservationCount), 0))  
               
-                    ###now you will put your analysis code here
-                    ##Then you will put your output table script here
-
-  GLM2 <- glmer(ObservationCount ~ doy + survey_year + ProtocolCode + (1 | RouteIdentifier),
-                family = poisson, data = ddf)
+  hist(sp.ddf$ObservationCount)
+  #Prepare variable<-
+  sp.ddf$SurveyAreaIdentifier<-as.numeric(factor(paste(sp.ddf$SurveyAreaIdentifier)))
+  sp.ddf$ProtocolCode<-as.numeric(factor(paste(sp.ddf$ProtocolCode)))
+  sp.ddf$scaleyear<-scale(sp.ddf$survey_year, center = TRUE, scale = TRUE)
+  
+  GLM1<- glmer(ObservationCount ~ scaleyear + (1 | SurveyAreaIdentifier), data = sp.ddf, family = poisson)
+  GLM2 <- glmer.nb(ObservationCount ~ scaleyear + (1 | SurveyAreaIdentifier), data = sp.ddf)
   
   # Get summary of the model
-  model_summary <- summary(GLM2)
+  model_summary1 <-summary(GLM1)
+  model_summary2 <- summary(GLM2)
+  
+  ###I have not done anything past getting the models to run###
   
   # Extract coefficients and statistics
   estimate <- model_summary$coefficients[2, 1]  # Estimate for predictor
